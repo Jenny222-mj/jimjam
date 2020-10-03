083996ad1ca970d100cd13517220d61bd7b63d94
Jenny222-mj <ogmbamalu@gmail.com>
2020-10-03 03:08:08 +0100

    third commit

--- list.js
+++ list.js
@@ -1,5 +1,9 @@
 function list() {
     var name = document.getElementById("name").value;
     document.getElementById("myInput").innerHTML = name;
+    var node = document.createElement("Li");
+    var textnode = createTextNode("cooking");
+    node.appendChild(textnode);
+    document.getElementById("myInput").appendChild(node);
 }
 
