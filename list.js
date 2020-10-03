661944ae21a6bb31186f73d711e9870d311264cc
AYAH <ayahluke20@gmail.com>
2020-10-03 04:32:45 +0100

    not an actual commit

--- list.js
+++ list.js
@@ -1,5 +1,5 @@
 function list() {
     var name = document.getElementById("name").value;
-    document.getElementById("myInput").innerHTML = name;
+    document.getElementById("myInput").innerHTML += name;
 }
 
